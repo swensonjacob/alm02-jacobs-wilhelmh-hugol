1274354b0ceac9a324951de6a6fe80d30a90ed21
jacobswenson <jacob.swenson@outlook.com>
2020-09-04 08:58:44 +0200

    port 8080

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     $("#fortune-btn").click(function () {
 
-        $.getJSON('http://localhost:8081/fortune?name=' + $('#full-name').val() +
+        $.getJSON('http://localhost:8080/fortune?name=' + $('#full-name').val() +
             '&nationality=' + $('#nationality').val() +
             '&age=' + $('#age').val(), function (data) {
 
